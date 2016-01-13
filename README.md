70f28465d0887c53262c2a44ded20296632e1aa3
Alon Niv <alon@cybereason.com>
2016-01-13 11:50:05 +0200

    Material commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # redux-action-store
 Save and load actions to persist state
+
+## Install for server-side use of redux
+```
+npm i redux-action-store
+```
+
+## Simple usage
+```js
+
+// store.js
+'use strict';
+
+const redux = require('redux');
+const app = require('./reducers');
+const middleware = require('./middleware');
+const ActionStore = require('redux-action-store');
+
+// attempt to load actions from JSON file, otherwise create a blank one
+const actionStore = ActionStore.fromJson('./actions.json', app);
+// add SIGINT and beforeExit hook to save back to JSON file
+actionStore.setPersistence('./actions.json');
+/*
+    You can also persist directly (sync) using: actionStore.toJson('./actions.json');
+*/
+
+module.exports = redux.applyMiddleware(...middleware, actionStore.storeActions)(actionStore.createStore)();
+```
+
+## Notes
+- If using the `setPersistence` hooks, please make sure to exit explicitly on `CTRL-C`.
+- As you should probably expect, replaying won't flare any side-effects (middleware).
---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+const redux = require('redux');
+
+class ActionStore {
+    constructor(reducer, history) {
+        this.history = history || [];
+        this.reducer = reducer;
+        this._store = redux.createStore(reducer);
+
+        this.createStore = this.createStore.bind(this);
+    }
+
+    static fromJson(path, reducer) {
+        try {
+            return new ActionStore(reducer, require(path).history);
+        } catch (err) {
+            return new ActionStore(reducer);
+        }
+    }
+
+    get storeActions() {
+        return store => next => action => {
+            this.record(action);
+            return next(action);
+        }
+    }
+
+    record(action) {
+        this.history.push(action);
+    }
+
+    createStore(reducer) {
+        this.history.forEach(this._store.dispatch.bind(this._store));
+        return redux.createStore(reducer || this.reducer, this._store.getState());
+    }
+
+    toJson(path) {
+        fs.writeFileSync(path, JSON.stringify({
+            history: this.history
+        }));
+    }
+
+    setPersistence(path) {
+        process.on('beforeExit', () => this.toJson(path));
+        process.on('SIGINT', () => {
+            this.toJson(path);
+        }); 
+        return this;
+    }
+}
+
+module.exports = ActionStore;
